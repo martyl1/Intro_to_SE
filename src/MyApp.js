7f6ce44ea826cfac4dbf6792a7a6e0fdf17dae28
Marty <mlange0132@gmail.com>
2021-04-07 09:48:50 -0700

    New repo due to git conflicts

---
+++ src/MyApp.js
@@ -0,0 +1,73 @@
+//import React from 'react'
+import React, {useState} from 'react'
+import Table from './Table'
+import Form from './Form'
+
+/*const characters = [
+    {
+        name: 'Charlie',
+        job: 'Janitor',
+    },
+    {
+        name: 'Mac',
+        job: 'Bouncer',
+    },
+    {
+        name: 'Dee',
+        job: 'Aspring actress',
+    },
+    {
+        name: 'Dennis',
+        job: 'Bartender',
+    },
+];
+
+function MyApp() {
+    return (
+        <div className="container">
+            <Table characterData={characters} />
+        </div>
+    );
+}*/
+function MyApp() {
+    const [characters, setCharacters] = useState([]);
+
+    function updateList(person) {
+        setCharacters([...characters, person]);
+    }
+
+    /*const [characters, setCharacters] = useState([
+        {
+            name: 'Charlie',
+            job: 'Janitor',
+        },
+        {
+            name: 'Mac',
+            job: 'Bouncer',
+        },
+        {
+            name: 'Dee',
+            job: 'Aspring actress',
+        },
+        {
+            name: 'Dennis',
+            job: 'Bartender',
+        },
+
+    ]);*/
+    return (
+        <div className="container">
+            <Table characterData={characters} removeCharacter={removeOneCharacter} />
+            <Form handleSubmit={updateList} />
+
+        </div>
+    )
+
+function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+    });
+    setCharacters(updated);
+  }
+}
+export default MyApp;
