f13189b63f299f0e8fae620cc256252289c3bc02
Marty <mlange0132@gmail.com>
2021-04-09 14:48:30 -0700

    added backend code

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,9 @@
 //import React from 'react'
-import React, {useState} from 'react'
+
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 
 /*const characters = [
     {
@@ -35,6 +37,24 @@ function MyApp() {
     function updateList(person) {
         setCharacters([...characters, person]);
     }
+    async function fetchAll(){
+        try {
+            const response = await axios.get('http://localhost:5000/users');
+            return response.data.users_list;
+        }
+        catch (error){
+            //We're not handling errors. Just logging into the console.
+            console.log(error);
+            return false;
+        }
+    }
+
+    useEffect(() => {
+        fetchAll().then( result => {
+            if (result)
+                setCharacters(result);
+        });
+    }, [] );
 
     /*const [characters, setCharacters] = useState([
         {
