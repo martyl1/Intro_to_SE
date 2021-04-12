7e47ee8bfd7c76ace6a073023854b8ea3fc9e05d
Marty <mlange0132@gmail.com>
2021-04-11 21:09:53 -0700

    delete to backend is not working

--- src/MyApp.js
+++ src/MyApp.js
@@ -35,7 +35,10 @@ function MyApp() {
     const [characters, setCharacters] = useState([]);
 
     function updateList(person) {
-        setCharacters([...characters, person]);
+        makePostCall(person).then( result => {
+            if (result)
+                setCharacters([...characters, result] );
+        });
     }
     async function fetchAll(){
         try {
@@ -44,6 +47,28 @@ function MyApp() {
         }
         catch (error){
             //We're not handling errors. Just logging into the console.
+            console.log(error);
+            return false;
+        }
+    }
+
+    async function makePostCall(person){
+        try {
+            const response = await axios.post('http://localhost:5000/users', person);
+            return response.data;
+        }
+        catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+
+    async function makeDeleteCall(person){
+        try {
+            const response = await axios.delete('http://localhost:5000/users', person);
+            return response;
+        }
+        catch (error) {
             console.log(error);
             return false;
         }
@@ -83,11 +108,15 @@ function MyApp() {
         </div>
     )
 
-function removeOneCharacter (index) {
+ function removeOneCharacter (index) {
     const updated = characters.filter((character, i) => {
         return i !== index
     });
-    setCharacters(updated);
+     makeDeleteCall(characters.character).then( result => {
+         if (result)
+             setCharacters( updated );
+     });
+
   }
 }
 export default MyApp;
